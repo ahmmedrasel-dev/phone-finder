e448f0bb91f58de5a68780187f93cc4f6853653a
Ahmmed Rasel <rahmmed.info@gmail.com>
2022-02-28 23:59:37 +0300

    display result

---
+++ js/app.js
@@ -0,0 +1,29 @@
+const getResult = () => {
+  const searchKeyword = document.getElementById('search-box').value;
+  const apiUrl = `https://openapi.programming-hero.com/api/phones?search=${searchKeyword}`;
+
+  fetch(apiUrl)
+    .then(response => response.json())
+    .then(data => displayResult(data.data));
+  // console.log(apiUrl)
+}
+
+const displayResult = phones => {
+  phones.forEach(phone => {
+    console.log(phone)
+    const displayContainer = document.getElementById('result-container');
+    const cardContetDiv = document.createElement('div');
+    cardContetDiv.classList.add('col-md-3')
+    cardContetDiv.innerHTML = `
+      <div class="card p-3">
+        <img src="${phone.image}" class="card-img-top w-50 mx-auto" alt="...">
+        <div class="card-body text-center">
+          <h5 class="card-title">Phone Name: ${phone.phone_name}</h5>
+          <h5 class="card-title">Brand Name:  ${phone.brand}</h5>
+          <a href="#" class="btn btn-primary" onclick="getDetails('')">More Details</a>
+        </div>
+      </div>
+    `;
+    displayContainer.appendChild(cardContetDiv)
+  })
+}
