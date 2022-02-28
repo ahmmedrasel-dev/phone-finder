6850a88ea7acd84ae99e9dfbf23800944a0b1db3
Ahmmed Rasel <rahmmed.info@gmail.com>
2022-03-01 00:34:26 +0300

    more-teails

--- js/app.js
+++ js/app.js
@@ -1,12 +1,15 @@
+// Getting The Data From Api
 const getResult = () => {
   const searchKeyword = document.getElementById('search-box').value;
   const apiUrl = `https://openapi.programming-hero.com/api/phones?search=${searchKeyword}`;
 
+  // Fatching Data From Api.
   fetch(apiUrl)
     .then(response => response.json())
     .then(data => displayResult(data.data));
 }
 
+// Display Search Result In Frontend.
 const displayResult = phones => {
   document.getElementById('result-container').textContent = '';
   let total = phones.slice(0, 20);
@@ -21,10 +24,16 @@ const displayResult = phones => {
         <div class="card-body text-center">
           <h5 class="card-title">Phone Name: ${phone.phone_name}</h5>
           <h5 class="card-title">Brand Name:  ${phone.brand}</h5>
-          <a href="#" class="btn btn-primary" onclick="getDetails('')">More Details</a>
+          <a href="#" class="btn btn-primary" onclick="moreDetails('${phone.slug}')">More Details</a>
         </div>
       </div>
     `;
     displayContainer.appendChild(cardContetDiv)
   })
+}
+
+// Get More Detisl Form Api
+const moreDetails = (slug) => {
+  const detailsUrl = `https://openapi.programming-hero.com/api/phone/${slug}`;
+  console.log(detailsUrl)
 }
